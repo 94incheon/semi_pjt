6bbb1d4e9d13a6a6642667cfaa6081fbbe4b27a6
Jaegu Kang <kjaegu9771@naver.com>
2020-03-31 00:44:17 +0900

    [add] Django Pjt

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # semi_pjt
+
+
+
+- `mygallery`
+  - Django 와 Api 를 활용한 간단한 사진 검색 서비스
+
---
+++ mygallery/pages/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+import requests
+
+
+# Create your views here.
+def pages(request):
+    return render(request, 'index.html')
+
+def gallery(request):
+    # Unsplash
+    text = request.GET['text'] # 여기 text 는 form 로 가져온 친구..
+    item = request.GET['item'] # 몇개 검색해줄지...
+    # 과제 제출용이니 내 key 숨기는법 몰라서 그냥 올립니다..흑흑.. env.. decouple... 몰라...
+    p_key = 'x7rOW3_hImx9UKrjl9xCxkYciiTEsftXNtpBahkJM3s' # 내 공개 키
+    api = f'https://api.unsplash.com/search/photos?query={text}&client_id={p_key}&per_page={item}'
+
+    a = requests.get(api).json()['results']
+
+    images = []
+    for i in range(len(a)):
+        aa = a[i]['urls']['regular']  
+        images.append(aa)
+
+
+    context = {
+        'text': text,
+        'images': images,
+    }
+    return render(request, 'gallery.html', context)
